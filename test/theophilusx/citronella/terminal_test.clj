8d7cce257d0d18633f69223bcd4c2442ae4943da
Tim Cross <theophilusx@gmail.com>
2020-10-14 18:55:12 +1100

    Add tests for terminal ns

--- test/theophilusx/citronella/terminal_test.clj
+++ test/theophilusx/citronella/terminal_test.clj
@@ -1,4 +1,46 @@
 (ns theophilusx.citronella.terminal-test
   (:require [theophilusx.citronella.terminal :as sut]
-            [clojure.test :as t]))
+            [clojure.test :refer [deftest testing is]]))
+
+(deftest get-terminal-tests
+  (testing "get-terminal no args"
+    (let [term (sut/get-terminal)]
+      (is (= (map? @term) true))
+      (is (= (:type @term) :auto))
+      (is (= (:open @term) true))
+      (is (= (:private @term) false))
+      (is (= (:cursor @term) [0 0]))
+      (is (= (:cursor-visible @term) true))
+      (is (= (:size @term) [80 24]))
+      (sut/close term)))
+  (testing "get-terminal :auto arg"
+    (let [term (sut/get-terminal {:type :auto})]
+      (is (= (map? @term) true))
+      (is (= (:type @term) :auto))
+      (is (= (:open @term) true))
+      (is (= (:private @term) false))
+      (is (= (:cursor @term) [0 0]))
+      (is (= (:cursor-visible @term) true))
+      (is (= (:size @term) [80 24]))
+      (sut/close term)))
+  (testing "get-terminal :text arg"
+    (let [term (sut/get-terminal {:type :text})]
+      (is (= (map? @term) true))
+      (is (= (:type @term) :text))
+      (is (= (:open @term) true))
+      (is (= (:private @term) false))
+      (is (= (:cursor @term) [0 0]))
+      (is (= (:cursor-visible @term) true))
+      (is (= (:size @term) [80 24]))
+      (sut/close term)))
+  (testing "get-terminal :text arg"
+    (let [term (sut/get-terminal {:type :gui})]
+      (is (= (map? @term) true))
+      (is (= (:type @term) :gui))
+      (is (= (:open @term) true))
+      (is (= (:private @term) false))
+      (is (= (:cursor @term) [0 0]))
+      (is (= (:cursor-visible @term) true))
+      (is (= (:size @term) [80 24]))
+      (sut/close term))))
 

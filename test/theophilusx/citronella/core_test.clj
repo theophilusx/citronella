aa4bb49fb8000335da14145e173630a2f9fa3aa9
Tim Cross <theophilusx@gmail.com>
2020-10-15 16:32:31 +1100

    Remove unnecessary core_test ns

--- test/theophilusx/citronella/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns theophilusx.citronella.core-test
-  (:require [clojure.test :refer :all]
-            [theophilusx.citronella.core :refer :all]))
-
-(deftest a-test
-  (testing "I pass."
-    (is (= 0 0))))
